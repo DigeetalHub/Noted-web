10f415478a002aa0d7a07a9d22e03042c8ddf48c
Klin-Coderse <obcbeats@gmail.com>
2023-02-03 23:10:56 +0300

    bye bye

--- context/AccordionContext.js
+++ context/AccordionContext.js
@@ -3,17 +3,20 @@ import { useState, createContext, useContext } from "react";
 
 const AccordionContext = createContext();
 
-const AccordionProvider = ({ children}) => {
-	const [isActive, setIsActive] = useState(null);
+const AccordionProvider = ({ children }) => {
+  const [isActive, setIsActive] = useState(null);
 
-	const values = {
-		isActive,
+  const values = {
+    isActive,
     setIsActive,
-	};
-	return <AccordionContext.Provider value={values}>{children}</AccordionContext.Provider>;
+  };
+  return (
+    <AccordionContext.Provider value={values}>
+      {children}
+    </AccordionContext.Provider>
+  );
 };
 
 const useToggle = () => useContext(AccordionContext);
 
 export { AccordionProvider, useToggle };
-
