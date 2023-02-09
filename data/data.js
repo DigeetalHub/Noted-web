195a89669b581602a98c533dd199291624325465
289volts <289volts@gmail.com>
2023-02-09 20:10:54 +0100

    [CHORE] .

--- data/data.js
+++ data/data.js
@@ -15,8 +15,6 @@ const privacyPolicy = [
 
 		description2:
 			"We will only process your personal data in accordance with applicable data protection and privacy laws. We need certain personal data in order to provide you with access to the website. If you registered with us, you will have been asked to tick to agree to provide this information in order to access our services, purchase our products, or view our content. This consent provides us with the legal basis we require under applicable law to process your data. You maintain the right to withdraw such consent at any time. If you do not agree to our use of your personal data in line with this Policy, please do not use our website.",
-		description3:
-			"We will only process your personal data in accordance with applicable data protection and privacy laws. We need certain personal data in order to provide you with access to the website. If you registered with us, you will have been asked to tick to agree to provide this information in order to access our services, purchase our products, or view our content. This consent provides us with the legal basis we require under applicable law to process your data. You maintain the right to withdraw such consent at any time. If you do not agree to our use of your personal data in line with this Policy, please do not use our website.",
 	},
 ];
 
