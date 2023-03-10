40adc0f9e4e6dc683d8263842b557662cfece1b7
Klin-Coderse <obcbeats@gmail.com>
2023-03-10 13:23:34 +0300

    waitlist

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -3,9 +3,15 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-if (!supabaseUrl || !supabaseKey) {
+if (!supabaseUrl) {
   throw new Error(
-    "Make sure to add your SUPABASE_URL and SUPABASE_ANON_KEY to your environment variables."
+    "Make sure to add your SUPABASE_URL to your environment variables."
+  );
+}
+
+if (!supabaseKey) {
+  throw new Error(
+    "Make sure to add yourSUPABASE_ANON_KEY to your environment variables."
   );
 }
 
