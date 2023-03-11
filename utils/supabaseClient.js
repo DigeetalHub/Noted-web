cfd23364edd13db53088ce6fc08c448ab3fcf412
289volts <289volts@gmail.com>
2023-03-11 20:49:22 +0100

    [FIX] Uncommented supabase code from the utils

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -1,18 +1,18 @@
-// import { createClient } from "@supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js";
 
-// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-// const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-// if (!supabaseUrl) {
-//   throw new Error(
-//     "Make sure to add your SUPABASE_URL to your environment variables."
-//   );
-// }
+if (!supabaseUrl) {
+  throw new Error(
+    "Make sure to add your SUPABASE_URL to your environment variables."
+  );
+}
 
-// if (!supabaseKey) {
-//   throw new Error(
-//     "Make sure to add yourSUPABASE_ANON_KEY to your environment variables."
-//   );
-// }
+if (!supabaseKey) {
+  throw new Error(
+    "Make sure to add yourSUPABASE_ANON_KEY to your environment variables."
+  );
+}
 
-// export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey);
