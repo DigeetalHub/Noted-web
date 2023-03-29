55de247278e3f70b4675893b9ff47c291ea2b011
289volts <289volts@gmail.com>
2023-03-29 01:00:14 +0100

    .

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import { Montserrat } from "@next/font/google";
 import { ScrollProvider } from "../context/ScrollContext";
 import { AccordionProvider } from "../context/AccordionContext";
 import { Fragment } from "react";
-import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Analytics } from "@vercel/analytics/react";
 import Layout from "../components/Layout";
