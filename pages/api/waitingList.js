798a8950c1f53843306444ba03b5647ef858c6d6
289volts <289volts@gmail.com>
2023-06-09 14:37:50 +0100

    .

--- pages/api/waitingList.js
+++ pages/api/waitingList.js
@@ -1,38 +1,38 @@
-import { MongoClient } from "mongodb";
+// import { MongoClient } from "mongodb";
 
-const client = new MongoClient(process.env.MONGO_URI);
+// const client = new MongoClient(process.env.MONGO_URI);
 
-export default async function handler(req, res) {
-	const { method } = req;
-	const { name, email } = req.body;
+// export default async function handler(req, res) {
+// 	const { method } = req;
+// 	const { name, email } = req.body;
 
-	switch (method) {
-		case "POST":
-			const connectDB = async () => {
-				await client.connect();
-				console.log("Connected successfully to server");
-				const db = client.db("Waitlist");
-				const collection = db.collection("users");
+// 	switch (method) {
+// 		case "POST":
+// 			const connectDB = async () => {
+// 				await client.connect();
+// 				console.log("Connected successfully to server");
+// 				const db = client.db("Waitlist");
+// 				const collection = db.collection("users");
 
-				if (name && email) {
-					await collection.insertOne({ name, email, createdAt: new Date().toLocaleDateString("en-NG") });
-					res.status(201).json({ success: true, message: "User added successfully" });
-				} else {
-					return res.status(400).json({ success: false, message: "Please fill in all fields" });
-				}
-			};
+// 				if (name && email) {
+// 					await collection.insertOne({ name, email, createdAt: new Date().toLocaleDateString("en-NG") });
+// 					res.status(201).json({ success: true, message: "User added successfully" });
+// 				} else {
+// 					return res.status(400).json({ success: false, message: "Please fill in all fields" });
+// 				}
+// 			};
 
-			connectDB()
-				.then((result) => {
-					res.status(201).json({ success: true, message: "User added successfully" });
-					console.log(result);
-				})
-				.catch((error) => {
-					res.status(400).json({ success: false, message: error.message });
-					console.log(error.message);
-				})
-				.finally(() => client.close());
+// 			connectDB()
+// 				.then((result) => {
+// 					res.status(201).json({ success: true, message: "User added successfully" });
+// 					console.log(result);
+// 				})
+// 				.catch((error) => {
+// 					res.status(400).json({ success: false, message: error.message });
+// 					console.log(error.message);
+// 				})
+// 				.finally(() => client.close());
 
-			break;
-	}
-}
+// 			break;
+// 	}
+// }
